896f94568295a98f1243dbf5cbcedeef82dda7c1
Irfanet <rachmatirfanm@gmail.com>
2019-11-30 08:18:02 +0700

    -jurnal (skeleton)

--- modules/jurnal/views/index.php
+++ modules/jurnal/views/index.php
@@ -1,10 +1,18 @@
+<?php
+$title = "Jurnal";
+$url = base_url() . 'jurnal/';
+?>
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
   <!-- Content Header (Page header) -->
   <section class="content-header">
     <h1>
-      Jurnal Keuangan
+      <?= $title ?>
     </h1>
+    <ol class="breadcrumb">
+      <li><a href="<?= base_url() ?>"><i class="fa fa-dashboard"></i> Home</a></li>
+      <li class="active"><?= $title ?></li>
+    </ol>
   </section>
 
   <!-- Main content -->
@@ -14,45 +22,56 @@
     </div>
     <div class="row">
       <div class="col-xs-12 col-md-12 col-lg-2">
-        <div class="box box-solid box-default">
+        <div class="box box-solid box-primary">
           <div class="box-header">
-            <h3 class="box-title">Tambah Transaksi</h3>
+            <h3 class="box-title">Tambah Data</h3>
           </div>
           <!-- /.box-header -->
           <div class="box-body">
             <center>
               <a class="btn btn-app btn-lg" data-toggle="modal" id="btn_add_modal" data-target="#modal_add">
-                <i class="fa fa-plus"></i> Tambah
+                <i class="fa fa-plus text-primary"></i> Tambah
               </a>
             </center>
           </div>
           <!-- /.box-body -->
         </div>
         <!-- /.box -->
+
+        <!-- /.box-header -->
+        <div class="box box-solid box-primary">
+          <div class="box-header with-border">
+            <h3 class="box-title">Keterangan</h3>
+          </div>
+          <div class="box-body no-padding">
+            <ul class="nav nav-pills nav-stacked">
+              <li><a href="#"><i class="fa fa-pencil text-primary"></i> <span>Edit</span></a></li>
+              <li><a href="#"><i class="fa fa-trash text-red"></i> <span> Hapus</span></a></li>
+            </ul>
+          </div>
+        </div>
+        <!-- /.col -->
       </div>
       <div class="col-xs-12 col-md-12 col-lg-10">
-        <div class="box box-solid box-default">
+        <div class="box box-solid box-primary">
           <div class="box-header">
-            <h3 class="box-title">Jurnal</h3>
+            <h3 class="box-title"><?= $title ?></h3>
             <div class="box-tools pull-right">
               <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
               </button>
             </div>
           </div>
-
-
           <!-- /.box-header -->
           <div class="box-body table-responsive">
             <table id="example2" class="table table-bordered table-hover">
               <thead>
                 <tr>
                   <th width="10px">No</th>
-                  <th>Tgl</th>
-                  <th>Akun</th>
+                  <th>Tanggal</th>
+                  <th>Kode Akun</th>
                   <th>Keterangan</th>
                   <th>Debit</th>
                   <th>Kredit</th>
-                  <!-- <th width="15%">Aksi</th> -->
                 </tr>
               </thead>
               <tbody id="show_data">
@@ -78,12 +97,74 @@
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" name="show_in_add">Tambah User</h4>
-        <h4 class="modal-title" name="show_in_edit">Edit User</h4>
+        <h4 class="modal-title" name="show_in_add">Tambah Data</h4>
+        <h4 class="modal-title" name="show_in_edit">Edit Data</h4>
       </div>
+      <!-- enctype="multipart/form-data" -->
       <form id="form_add" data-parsley-validate class="form-horizontal form-label-left">
         <div class="modal-body">
-          <input type="hidden" id="id_user" name="id_user">
+          <input type="hidden" id="id" name="id">
+          <input type="hidden" name="old_image" id="old_image">
+          <div class="row">
+            <div class="form-group">
+              <label class="control-label col-md-4 col-sm-4 col-xs-4" for="tgl">Nama Muzaki <span class="required">*</span>
+              </label>
+              <div class="col-md-6 col-sm-6 col-xs-6">
+                <input type="text" id="tgl" name="tgl" required class="form-control col-md-7 col-xs-12">
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="form-group">
+              <label class="control-label col-md-4 col-sm-4 col-xs-4" for="kd_akun">kd_akun <span class="required">*</span>
+              </label>
+              <div class="col-md-6 col-sm-6 col-xs-6">
+                <input type="text" id="kd_akun" name="kd_akun" required class="form-control col-md-7 col-xs-12">
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="form-group">
+              <label class="control-label col-md-4 col-sm-4 col-xs-4" for="keterangan">No. HP <span class="required">*</span>
+              </label>
+              <div class="col-md-6 col-sm-6 col-xs-6">
+                <input type="text" id="keterangan" name="keterangan" required class="form-control col-md-7 col-xs-12">
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="form-group">
+              <label class="control-label col-md-4 col-sm-4 col-xs-4" for="debit">Jenis Muzaki <span class="required">*</span>
+              </label>
+              <div class="col-md-6 col-sm-6 col-xs-6">
+                <select type="text" class="form-control" id="debit" name="debit" required>
+                  <option value="Pribadi">Pribadi</option>
+                  <option value="Entitas">Entitas</option>
+                  <option value="Lembaga">Lembaga</option>
+                </select>
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="form-group">
+              <label class="control-label col-md-4 col-sm-4 col-xs-4" for="kredit">kredit <span class="required">*</span>
+              </label>
+              <div class="col-md-6 col-sm-6 col-xs-6">
+                <select type="text" class="form-control" id="kredit" name="kredit" required>
+                  <option value="Islam">Islam</option>
+                  <option value="Kristen">Kristen</option>
+                  <option value="Katholik">Katholik</option>
+                  <option value="Hindu">Hindu</option>
+                  <option value="Budha">Budha</option>
+                </select>
+              </div>
+            </div>
+          </div>
+
           <div class="row">
             <div class="form-group">
               <label class="control-label col-md-4 col-sm-4 col-xs-4" for="email">Email <span class="required">*</span>
@@ -93,69 +174,37 @@
               </div>
             </div>
           </div>
-          <br>
+
           <div class="row">
             <div class="form-group">
-              <label class="control-label col-md-4 col-sm-4 col-xs-4" for="username">Username <span class="required">*</span>
+              <label class="control-label col-md-4 col-sm-4 col-xs-4" for="no_ktp">No. KTP <span class="required">*</span>
               </label>
               <div class="col-md-6 col-sm-6 col-xs-6">
-                <input type="text" id="username" name="username" required class="form-control col-md-7 col-xs-12">
+                <input type="text" id="no_ktp" name="no_ktp" required class="form-control col-md-7 col-xs-12">
               </div>
             </div>
           </div>
-          <br name="show_in_add">
-          <div class="row" name="show_in_add">
-            <div class="form-group">
-              <label class="control-label col-md-4 col-sm-4 col-xs-4" for="password1" name="password1">Password <span class="required">*</span>
-              </label>
-              <div class="col-md-6 col-sm-6 col-xs-6">
-                <input type="password" id="password1" name="password1" required class="form-control col-md-7 col-xs-12">
-              </div>
-            </div>
-          </div>
-          <br name="show_in_add">
-          <div class="row" name="show_in_add">
-            <div class="form-group">
-              <label class="control-label col-md-4 col-sm-4 col-xs-4" for="password2" name="password2">Konfirmasi Password <span class="required">*</span>
-              </label>
-              <div class="col-md-6 col-sm-6 col-xs-6">
-                <input type="password" id="password2" name="password2" required class="form-control col-md-7 col-xs-12">
-              </div>
-            </div>
-          </div>
-          <br>
+
           <div class="row">
             <div class="form-group">
-              <label class="control-label col-md-4 col-sm-4 col-xs-4" for="level">Level <span class="required">*</span>
+              <label class="control-label col-md-4 col-sm-4 col-xs-4" for="keterangan">Keterangan <span class="required">*</span>
               </label>
               <div class="col-md-6 col-sm-6 col-xs-6">
-                <select class="form-control" name="level" id="level">
-                  <optgroup label="Admin">
-                    <option value="1">Super Admin</option>
-                    <option value="2">Manager</option>
-                    <option value="3">HRD</option>
-                  </optgroup>
-                  <optgroup label="Operator">
-                    <option value="4">Operator Absen & SPL</option>
-                    <option value="5">Operator Payroll</option>
-                  </optgroup>
-                </select>
+                <input type="text" id="keterangan" name="keterangan" required class="form-control col-md-7 col-xs-12">
               </div>
             </div>
           </div>
-          <br>
-          <div class="row" name="show_in_edit">
+
+          <div class="row">
             <div class="form-group">
-              <label class="control-label col-md-4 col-sm-4 col-xs-4" for="is_active">Status <span class="required">*</span>
+              <label class="control-label col-md-4 col-sm-4 col-xs-4" for="foto">Foto <span class="required">*</span>
               </label>
               <div class="col-md-6 col-sm-6 col-xs-6">
-                <select class="form-control" name="is_active" id="is_active">
-                  <option value="1">Aktif</option>
-                  <option value="0">Tidak Aktif</option>
-                </select>
+                <input type="file" id="foto" name="foto" required>
               </div>
             </div>
           </div>
+
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-default" data-dismiss="modal">Batal</button>
@@ -179,7 +228,7 @@
         <h4 class="modal-title">Hapus Data</h4>
       </div>
       <div class="modal-body">
-        <input type="hidden" name="id_data" id="id_data">
+        <input type="hidden" name="kd_data" id="kd_data">
         <h5>Apakah Anda Yakin ?</h5>
       </div>
       <div class="modal-footer">
@@ -199,14 +248,12 @@
     tampil_data();
     var kondisi;
 
-
-
     //fungsi tampil data
     function tampil_data() {
       $.ajax({
         type: 'ajax',
-        url: '<?= base_url() ?>user/get_data',
-        async: false,
+        url: '<?= $url ?>getData',
+        async: true,
         dataType: 'json',
         success: function(data) {
           $('#example2').dataTable().fnDestroy();
@@ -216,14 +263,11 @@
           for (i = 0; i < data.length; i++) {
             html += '<tr>' +
               '<td>' + no++ + '</td>' +
-              '<td>' + data[i].email + '</td>' +
-              '<td>' + data[i].username + '</td>' +
-              '<td>' + data[i].level + '</td>' +
-              '<td>' + data[i].is_active + '</td>' +
-              '<td style="text-align:center;">' +
-              '<a href="javascript:;" class="btn btn-info btn-xs item_edit" data="' + data[i].id_user + '"><?= $this->lang->line('bt_edit'); ?></a>' + ' ' +
-              '<a href="javascript:;" class="btn btn-danger btn-xs item_hapus" data="' + data[i].id_user + '"><?= $this->lang->line('bt_hapus'); ?></a>' +
-              '</td>' +
+              '<td>' + data[i].tgl + '</td>' +
+              '<td>' + data[i].kd_akun + '</td>' +
+              '<td>' + data[i].keterangan + '</td>' +
+              '<td>' + data[i].debit + '</td>' +
+              '<td>' + data[i].kredit + '</td>' +
               '</tr>';
           }
           $('#show_data').html(html);
@@ -245,11 +289,8 @@
       $('#form_add')[0].reset();
       $('[name="show_in_add"]').show();
       $('[name="show_in_edit"]').hide();
-      $('#email').attr('readonly', false);
-      $('#username').attr('readonly', false);
-      $('#modal_add').on('shown.bs.modal', function() {
-        $('#email').focus()
-      });
+      $('#tgl').attr('readonly', false);
+      $('#kd_akun').attr('readonly', false);
     });
 
     //TOMBOL EDIT -> GET KODE & ATUR HIDE AND SHOW
@@ -257,25 +298,25 @@
       kondisi = "edit";
       var id = $(this).attr('data');
       $.ajax({
+        async: true,
         type: "GET",
-        url: "<?= base_url() ?>user/get_kode",
+        url: "<?= $url ?>getDataByKode",
         dataType: "JSON",
         data: {
           id: id
         },
         success: function(data) {
-          $.each(data, function(id_user, email, username, level, is_active) {
+          $.each(data, function(id, tgl, kd_akun, keterangan, debit, kredit) {
             $('#modal_add').modal('show');
             $('[name="show_in_add"]').hide();
             $('[name="show_in_edit"]').show();
-            $('#id_user').val(data.id_user);
-            $('#email').val(data.email).attr('readonly', true);
-            $('#username').val(data.username).attr('readonly', false);
-            $('#level').val(data.level);
-            $('#is_active').val(data.is_active);
-            $('#modal_add').on('shown.bs.modal', function() {
-              $('#username').focus()
-            });
+            $('#old_image').val(data.foto);
+            $('#id').val(data.id);
+            $('#tgl').val(data.tgl).attr('readonly', true);
+            $('#kd_akun').val(data.kd_akun);
+            $('#keterangan').val(data.keterangan);
+            $('#debit').val(data.debit);
+            $('#kredit').val(data.kredit);
           });
         }
       });
@@ -286,27 +327,26 @@
     $('#show_data').on('click', '.item_hapus', function() {
       var id = $(this).attr('data');
       $('#modal_delete').modal('show');
-      $('[name="id_data"]').val(id);
+      $('#kd_data').val(id);
     });
 
     //SIMPAN DATA
     $('#btn_simpan').on('click', function() {
+      var myform = new FormData($('#form_add')[0]);
       if (kondisi == "tambah") {
         $.ajax({
+          async: true,
           type: "POST",
-          url: "<?= base_url() ?>user/simpan_data",
+          url: "<?= $url ?>setData",
           dataType: "JSON",
-          data: {
-            email: $('#email').val(),
-            username: $('#username').val(),
-            password1: $('#password1').val(),
-            password2: $('#password2').val(),
-            level: $('#level').val()
-          },
+          data: myform,
+          cache: false,
+          processData: false,
+          contentType: false,
           success: function(data) {
             if (data.success == true) {
               $('#info').append('<div class="alert alert-success"><i class="fa fa-check"></i>' +
-                ' <?= $this->lang->line('notif_simpan'); ?> ' + '</div>');
+                ' <b>Bershasil ! </b>Data telah disimpan ! ' + '</div>');
               $('.form-group').removeClass('has-error')
                 .removeClass('has-success');
               $('.text-danger').remove();
@@ -336,19 +376,18 @@
       //EDIT DATA
       else if (kondisi == "edit") {
         $.ajax({
+          async: true,
           type: "POST",
-          url: "<?= base_url() ?>user/update_data",
+          url: "<?= $url ?>updateData",
           dataType: "JSON",
-          data: {
-            id_user: $('#id_user').val(),
-            username: $('#username').val(),
-            level: $('#level').val(),
-            is_active: $('#is_active').val()
-          },
+          data: myform,
+          cache: false,
+          processData: false,
+          contentType: false,
           success: function(data) {
             if (data.success == true) {
               $('#info').append('<div class="alert alert-success"><i class="fa fa-check"></i>' +
-                ' <?= $this->lang->line('notif_update'); ?> ' + '</div>');
+                ' <b>Berhasil !</b> Data telah diedit !' + '</div>');
               $('.form-group').removeClass('has-error')
                 .removeClass('has-success');
               $('.text-danger').remove();
@@ -380,10 +419,11 @@
 
     //HAPUS DATA
     $('#btn_hapus').on('click', function() {
-      var kode = $('#id_data').val();
+      var kode = $('#kd_data').val();
       $.ajax({
+        async: true,
         type: "POST",
-        url: "<?= base_url() ?>user/hapus_data",
+        url: "<?= $url ?>deleteData",
         dataType: "JSON",
         data: {
           kode: kode
@@ -392,7 +432,7 @@
           $('#modal_delete').modal('hide');
           tampil_data();
           $('#info').append('<div class="alert alert-danger"><i class="fa fa-trash-o"></i>' +
-            ' <?= $this->lang->line('notif_hapus'); ?>' + '</div>');
+            ' <b>Berhasil !</b> Data telah dihapus !</b>' + '</div>');
           $('.alert-danger').delay(500).show(1000, function() {
             $(this).delay(2000).slideUp(500, function() {
               $(this).remove();
